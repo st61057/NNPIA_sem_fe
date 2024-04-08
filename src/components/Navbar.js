718ba91504be6aa72e28cc10b380e1e2e97b6703
st61057 <st61057@upce.cz>
2024-04-08 20:07:18 +0200

    editing navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,52 +3,32 @@ import {HashLink as Link} from 'react-router-hash-link';
 
 function Navbar({loggedIn}) {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="/">Barbershop</a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse"
-                    data-target="#navbarSupportedContent">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-
+        <nav className="navbar bg-light">
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                {loggedIn === false ?
                 <ul className="navbar-nav mr-auto">
-                    <li className="nav-item active">
-                        <Link className="nav-link" to="/">Úvodní strana</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/reservation">Rezervace</Link>
-                    </li>
+                    <li className="nav-item active"><Link className="nav-link" to="/">Barbershop</Link></li>
+                    <li className="nav-item"><Link className="nav-link" to="/reservation">Reservation</Link></li>
+                    {loggedIn ?
+                        <>
+                            <li className="nav-item"><Link className="nav-link" to="/summary_procedures">Edit procedures</Link></li>
+                            <li className="nav-item"><Link className="nav-link" to="/manage_reservation">Summary reservations</Link></li>
+                        </>
+                        :
+                        <></>
+                    }
                 </ul>
-                :
-                <ul className="navbar-nav mr-auto">
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/adm_procedures">Úprava procedur</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/adm_reservation">Přehled rezervací</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/administration">Administrace uživatelů</Link>
-                    </li>
-                </ul>
-                }
             </div>
 
             {loggedIn ?
                 <ul className="nav navbar-nav navbar-right">
-                    <li className="nav-item"><Link className="nav-link" to="/edit">Úprava údajů</Link></li>
-                    <li className="nav-item"><Link className="nav-link" to="/logout">Odhlášení</Link></li>
-                    <li className="nav-item"><Link className="nav-link" to="/registration">Registrace</Link></li>
-
+                    <li className="nav-item"><Link className="nav-link" to="/logout">Logout</Link></li>
+                    <li className="nav-item"><Link className="nav-link" to="/adduser">Adding user</Link></li>
                 </ul>
                 :
                 <ul className="nav navbar-nav navbar-right">
-                    <li className="nav-item"><Link className="nav-link" to="/login">Přihlášení</Link></li>
+                    <li className="nav-item"><Link className="nav-link" to="/login">Login</Link></li>
                 </ul>
             }
-
-
         </nav>
     )
 }
