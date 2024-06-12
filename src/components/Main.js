8e5923eeb3c7049a2d479097a63eb49d69a91437
st61057 <st61057@upce.cz>
2024-06-12 11:23:50 +0200

    cleaning main class

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,4 @@
 import './style.css'
-import {Link} from "react-router-dom";
 import {useEffect, useState} from "react";
 import ProcedureList from "./ProceduresList";
 import ClientReservationService from "../service/ClientReservationService";
@@ -18,19 +17,16 @@ function Main() {
     return (
         <div>
             <div className="card">
-                <h1 className="h1">Naše procedury!</h1>
+                <h1 className="h1">Our procedures</h1>
                 <div className="card-body w-50 mx-auto" id="procedures">
                     {procedures.length
                         ? procedures.map(row => <ProcedureList clickable={false} key={row.name} row={row}
                                                                setProcedure={() => {
                                                                }}/>)
-                        : <h3>Žádné procedury nejsou dostupné nyní, zkuste to prosím později.</h3>
+                        : <h3>We currently do not offer any procedures</h3>
                     }
                 </div>
             </div>
-            <Link to="/reservation">Create reservation</Link>
-            <Link to="/login">Login</Link>
-            <Link to="/edituser" >Uprav uživatele</Link>
 
         </div>
     )
