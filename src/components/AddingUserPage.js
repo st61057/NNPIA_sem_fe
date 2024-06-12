03d64204303034f49e083dcbfb59a8b9f8b9634f
st61057 <st61057@upce.cz>
2024-06-12 09:59:03 +0200

    correcting name

--- src/components/AddingUserPage.js
+++ src/components/AddingUserPage.js
@@ -3,18 +3,18 @@ import {useEffect, useState} from "react";
 import AuthService from "../service/AuthService";
 import {useNavigate} from 'react-router-dom';
 
-function RegistrationPage(props) {
+function AddingUserPage() {
     const [username, setUsername] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const navigate = useNavigate();
 
 
-    const registration = (e) => {
+    const addUser = (e) => {
         e.preventDefault();
 
         const createdUser = {username, email, password};
-        AuthService.registration(createdUser).then(res => {
+        AuthService.addUser(createdUser).then(res => {
             if (res.status === 200) {
                 navigate('/');
             } else {
@@ -38,34 +38,28 @@ function RegistrationPage(props) {
 
     return (
         <div className="container w-50 mx-auto mt-4">
-            <h3>Login</h3>
+            <h3>Add user</h3>
             <hr/>
             <form>
                 <div className="form-group row m-3">
                     <label className="col-sm-3 col-form-label">Username</label>
-                    <div className="col-sm-7">
-                        <input type="text" className="form-control" placeholder="Enter username" name="username"
-                               value={username} onChange={handleChangeUser} required={true}/>
-                    </div>
+                    <input type="text" className="form-control" placeholder="Enter username" name="username"
+                           value={username} onChange={handleChangeUser} required={true}/>
                 </div>
                 <div className="form-group row m-3">
                     <label className="col-sm-3 col-form-label">Email</label>
-                    <div className="col-sm-7">
-                        <input type="email" className="form-control" placeholder="Email" name="email"
-                               value={email} onChange={handleChangeEmail} required={true}/>
-                    </div>
+                    <input type="email" className="form-control" placeholder="Email" name="email"
+                           value={email} onChange={handleChangeEmail} required={true}/>
                 </div>
                 <div className="form-group row m-3">
                     <label className="col-sm-3 col-form-label">Password</label>
-                    <div className="col-sm-7">
-                        <input type="password" className="form-control" placeholder="Password" name="password"
-                               value={password} onChange={handleChangePass} required={true}/>
-                    </div>
+                    <input type="password" className="form-control" placeholder="Password" name="password"
+                           value={password} onChange={handleChangePass} required={true}/>
                 </div>
-                <button type="submit" className="btn btn-primary w-25" onClick={registration}>Login</button>
+                <button type="submit" className="btn btn-primary w-25" onClick={addUser}>Add user</button>
             </form>
         </div>
     )
 }
 
-export default RegistrationPage;
+export default AddingUserPage;
